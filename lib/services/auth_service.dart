a7a5180c4802d1e887d7cd57bcdf23311c77701d
tpark02 <tpark01@hotmail.com>
2025-08-31 20:50:09 +0900

    fix(ios): correct Info.plist and add Google Sign-In URL scheme
    - Fixed malformed closing tag in Info.plist (extra </array>)
    - Added CFBundleURLTypes with REVERSED_CLIENT_ID
    - Added LSApplicationQueriesSchemes ("google")
    - Validated with `plutil -lint` and rebuilt pods

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io' show Platform;
+import 'package:TFA/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
@@ -5,15 +7,24 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class AuthService {
   final _auth = FirebaseAuth.instance;
 
+  // One GoogleSignIn instance; give iOS its clientId, others get null.
+  final GoogleSignIn _gsi = GoogleSignIn(
+    scopes: ['email'],
+    clientId: (!kIsWeb && Platform.isIOS)
+        ? DefaultFirebaseOptions.currentPlatform.iosClientId
+        : null,
+  );
+
   Stream<User?> authState() => _auth.authStateChanges();
 
   Future<UserCredential> signInWithGoogle() async {
     if (kIsWeb) {
       final provider = GoogleAuthProvider()
         ..setCustomParameters({'prompt': 'select_account'});
-      return await _auth.signInWithPopup(provider);
+      return _auth.signInWithPopup(provider);
     }
-    final gUser = await GoogleSignIn(scopes: ['email']).signIn();
+
+    final gUser = await _gsi.signIn();
     if (gUser == null) {
       throw FirebaseAuthException(code: 'aborted', message: 'User cancelled');
     }
@@ -22,12 +33,12 @@ class AuthService {
       idToken: gAuth.idToken,
       accessToken: gAuth.accessToken,
     );
-    return await _auth.signInWithCredential(cred);
+    return _auth.signInWithCredential(cred);
   }
 
   Future<void> signOut() async {
     try {
-      await GoogleSignIn().signOut();
+      await _gsi.signOut();
     } catch (_) {}
     await _auth.signOut();
   }
